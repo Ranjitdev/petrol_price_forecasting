59c5ccc3575808379c25aeaf7a6ba341f07cfb59
Ranjitdev <rkrnjtkundu10@gmail.com>
2023-11-05 18:15:43 +0530

    Graph generator structure

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from isort import file
 import streamlit as st
 from src.components.data_ingesion import DataIngesion
 from src.utils import *
 
+# Sidebar
 with st.sidebar:
-    option = st.radio(
-        ['Forecast', 'Old Prices', 'Graphs'],
+    option = st.radio('Select option',
+        ['Forecast', 'Data', 'Graphs', 'Developer'],
         captions=[
-            'Forecasting price & Enter new data', 'View excel file', 'Graphs and Charts'
+            'Forecasting price & New Entry', 'View excel file of prices', 'Graphs and Statistics of data', 'Developer option'
         ]
     )
 
+# Forecasting and new data entry feature
 if option == 'Forecast':
     pass
 
-elif option == 'Old Prices':
+# Data view and download
+elif option == 'Data':
     all_data, actual_data = DataIngesion().get_data('database')
     pivot_table = make_pivot(all_data)
 
@@ -32,7 +34,12 @@ elif option == 'Old Prices':
         st.dataframe(pivot_table)
 
         pivot_table_df = pivot_table.to_csv()
-        st.download_button(label='Download', data=pivot_table_df, file_name='Data.csv', mime='text/csv')
+        st.download_button(label='Download', data=pivot_table_df, file_name='Summary.csv', mime='text/csv')
 
+# Graph plotting and graphical statistics
 elif option == 'Graphs':
+    pass
+
+# Logs, Files and others
+elif option == 'Developer':
     pass
